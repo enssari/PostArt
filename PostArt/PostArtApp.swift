47fe8918889f0e8ab1f8dd36903e1acac2fa77ac
Enes <terdodios@gmail.com>
2024-09-06 14:06:49 +0300

    Initial Commit

---
+++ PostArt/PostArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  PostArtApp.swift
+//  PostArt
+//
+//  Created by Enes Sarı on 9/6/24.
+//
+
+import SwiftUI
+
+@main
+struct PostArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
